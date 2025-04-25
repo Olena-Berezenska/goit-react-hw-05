a60720e247d3203132202a550c067f131b658327
OlenaB <elena.yefimchuk@gmail.com>
2025-04-25 10:34:54 -0600

    location fix

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -2,15 +2,14 @@ import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
 const MovieList = ({ movies }) => {
+  const location = useLocation();
+
   return (
     <div>
       <ul>
         {movies.map(item => (
           <li key={item.id}>
-            <Link
-              to={`/movies/${item.id}`}
-              state={{ from: `${location.pathname}${location.search}` }}
-            >
+            <Link to={`/movies/${item.id}`} state={{ from: location }}>
               {item.title}
             </Link>
           </li>
